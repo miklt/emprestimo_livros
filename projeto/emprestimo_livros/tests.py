7684fabd147b7e55979d9effba6fcd2e1049eb60
michelet <michelet@usp.br>
2022-09-30 15:39:24 -0300

    Criação de modelos (entidades) Livro, Usuario e Emprestimo. Criação de testes para verificar o armazenamento dos objetos no banco de dados.

--- projeto/emprestimo_livros/tests.py
+++ projeto/emprestimo_livros/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
 # Create your tests here.
+from emprestimo_livros.models import Livro, Usuario, Emprestimo
+
+class LivroModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Livro.objects.create(titulo='Os Irmãos Karamazov',isbn='000000')
+    def test_criacao_id(self):
+        livro_1 = Livro.objects.get(titulo='Os Irmãos Karamazov')
+        self.assertEqual(livro_1.id, 1)
+class UsuarioModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Usuario.objects.create(nome="Michelet")
+
+    def test_criacao_id(self):
+        usuario_1 = Usuario.objects.get(nome="Michelet")
+        self.assertEqual(usuario_1.id, 1)
+
+class EmprestimoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Usuario.objects.create(nome="Michelet")
+        Livro.objects.create(titulo='Os Irmãos Karamazov')
+        livro_1 = Livro.objects.get(titulo='Os Irmãos Karamazov')
+        usuario_1 = Usuario.objects.get(nome="Michelet")
+        Emprestimo.objects.create(livro=livro_1, usuario=usuario_1)
+    def test_criacao_emprestimo_id(self):
+        emprestimo_1 = Emprestimo.objects.get(id=1)
+        self.assertEqual(emprestimo_1.usuario.id,1)
